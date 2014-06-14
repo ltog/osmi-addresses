281a255149b8cecc4899913cacf441307f49d7ae
Lukas Toggenburger <lukas.toggenburger@htwchur.ch>
2014-06-15 01:05:25 +0200

    Cosmetic for the output of the finishing message

--- osmi/main.cpp
+++ osmi/main.cpp
@@ -90,7 +90,7 @@ int main(int argc, char* argv[]) {
 	std::cout << std::endl;
 	mem_helper.print_max();
 
-	std::cout << "\nsoftware finished properly\n" << std::endl;
+	std::cout << std::endl << "software finished properly" << std::endl << std::endl;
 	return 0;
 }
 
