9f5b372a5337c7480a4dcf161cc1d65e9c4cb1a2
Lukas Toggenburger <lukas.toggenburger@htwchur.ch>
2014-12-30 14:21:33 -0800

    Replace 'given' and 'when' (fix #36)

--- test/test_engine.pl
+++ test/test_engine.pl
@@ -11,50 +11,41 @@ my $op       = $ARGV[3];
 my $expected = $ARGV[4];
 my $result;
 
-given($op) {
-	when('=') {
-		$result = qx(sqlite3 $file '$query');
-		chomp $result;
-	
-		&print_result;
-	}
-	
-	when(/bbox/) {
-		my @values = split(';', $query);
-		my $table     = $values[0];
-		my $left      = $values[1]; 
-		my $right     = $values[2];
-		my $bottom    = $values[3];
-		my $top       = $values[4];
-		my $condition = $values[5];
-		my $assembled_query;
+if ($op eq '=') {
+	$result = qx(sqlite3 $file '$query');
+	chomp $result;
 
-		given($op) {
-			# queries from http://giswiki.hsr.ch/SpatiaLite_-_Tipps_und_Tricks
-			when('outofbbox') {
-				$assembled_query = "SELECT COUNT(*) FROM $table WHERE ROWID IN (SELECT pkid FROM idx_${table}_GEOMETRY WHERE (xmin>$right OR xmax<$left OR ymin>$top OR ymax<$bottom) ";
-			}
-			when('inbbox') {
-				$assembled_query = "SELECT COUNT(*) FROM $table WHERE ROWID IN (SELECT pkid FROM idx_${table}_GEOMETRY WHERE (xmin<$left AND xmax>$right AND ymin<$bottom AND ymax>$top) ";
-			}
-			default {
-				&report_unknown_operator;
-			}
-		}
-		if (defined $condition) {
-			$assembled_query .= " AND $condition)"; 
-		} else {
-			$assembled_query .= ")";
-		}
-		$result = qx(sqlite3 $file '$assembled_query');
-		chomp $result;
-	
-		&print_result;
-	} 
-	
-	default {
+	&print_result;
+} elsif ($op =~ /bbox/) {
+	my @values = split(';', $query);
+	my $table     = $values[0];
+	my $left      = $values[1]; 
+	my $right     = $values[2];
+	my $bottom    = $values[3];
+	my $top       = $values[4];
+	my $condition = $values[5];
+	my $assembled_query;
+
+	if ($op eq 'outofbbox') {
+		# queries from http://giswiki.hsr.ch/SpatiaLite_-_Tipps_und_Tricks
+		$assembled_query = "SELECT COUNT(*) FROM $table WHERE ROWID IN (SELECT pkid FROM idx_${table}_GEOMETRY WHERE (xmin>$right OR xmax<$left OR ymin>$top OR ymax<$bottom) ";
+	} elsif ($op eq 'inbbox') {
+		$assembled_query = "SELECT COUNT(*) FROM $table WHERE ROWID IN (SELECT pkid FROM idx_${table}_GEOMETRY WHERE (xmin<$left AND xmax>$right AND ymin<$bottom AND ymax>$top) ";
+	} else {
 		&report_unknown_operator;
 	}
+
+	if (defined $condition) {
+		$assembled_query .= " AND $condition)"; 
+	} else {
+		$assembled_query .= ")";
+	}
+	$result = qx(sqlite3 $file '$assembled_query');
+	chomp $result;
+
+	&print_result;
+} else {
+	&report_unknown_operator;
 }
 
 
