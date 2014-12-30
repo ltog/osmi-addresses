712780e1733fe43ca0a5746c35b75bdd85736f37
Lukas Toggenburger <lukas.toggenburger@htwchur.ch>
2014-12-30 13:09:30 -0800

    Add file single_click_starter.sh for easier running of tests

---
+++ test/single_click_starter.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# get the directory of the script (according to http://stackoverflow.com/a/246128 )
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+cd $DIR
+
+# generate 'pos-osmi-testzone.osm'
+./makeidpositive.sh osmi-testzone.osm
+
+# remove old file 'out.sqlite'
+rm out.sqlite
+
+# read 'pos-osmi-testzone.osm' and generate file 'out.sqlite'
+../osmi/osmi pos-osmi-testzone.osm
+
+# create spatial indices for 'out.sqlite'
+../create_spatial_indices.sh out.sqlite
+
+# run tests on 'out.sqlite'
+./run_tests.sh out.sqlite
