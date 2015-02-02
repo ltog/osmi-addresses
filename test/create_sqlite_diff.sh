6d5d80ceae5e32e4cc974d64072a29875f6c9d7a
Lukas Toggenburger <lukas.toggenburger@htwchur.ch>
2015-02-03 00:17:24 +0100

    Enhance create_sqlite_diff.sh (but doesn't work yet)

--- test/create_sqlite_diff.sh
+++ test/create_sqlite_diff.sh
@@ -4,6 +4,7 @@
 # Website: https://github.com/ltog/osmi-addresses
 
 description="This tool reads two spatialite files and creates a third containing the difference (deleted and newly added rows) of the tables of those two files."
+dummydb='dummydbdeleteme'
 
 # make sure exactly 3 arguments are given
 if [ $# -ne 3 ]; then
@@ -41,14 +42,40 @@ then
 	exit 4
 fi
 
-attach="ATTACH DATABASE '$1' AS db1; ATTACH DATABASE '$2' AS db2; "
+attachdbs="ATTACH DATABASE '$1' AS db1; ATTACH DATABASE '$2' AS db2; ATTACH DATABASE '$3' AS db3;"
 
 # for each table write the differences to a new database
 while read -r table; do
 	echo "Processing table $table"
 
+	# create new geometry tables in db3
+	# variant 1: manually call AddGeometryColumn: http://stackoverflow.com/a/23366622 https://www.gaia-gis.it/spatialite-2.3.0/spatialite-sql-2.3.0.html#p16
+	# variant 2: clone table
+
+	# get schema
+	schema=$(spatialite $1 ".schema $table" | grep 'CREATE TABLE' | grep "$table")
+
+	# get geometry type
+	geometry= #TODO
+
+	# TODO: remove geometry column from schema (sqlite/spatialite can't rename/remove columns)
+	schema=
+
+	# create new tables
+	spatialite $3 "$schema"
+	spatialite $3 "$schema"
+
+	#CREATE TABLE 'blub_deleted' (ogc_fid INTEGER PRIMARY KEY);
+	#CREATE TABLE 'blub_added' (ogc_fid INTEGER PRIMARY KEY);
+	# TODO: how to copy all columns except geometry? add all and delete geometry column again?
+
+	# add geometry column
+	spatialite $3 "AddGeometryColumn('${table}_deleted', 'geometry', '4326', '$geometry');"
+	spatialite $3 "AddGeometryColumn('${table}_added',   'geometry', '4326', '$geometry');"
+
 	#TODO: calculate differences and write them to db3
-	spatialite dummydbdeleteme "$attach ; select astext(geometry) from db1.ausschnitt limit 1"
+	spatialite $dummydb "$attachdbs INSERT INTO db3.'${table}_deleted' SELECT * from db1.'${table}' WHERE NOT EXISTS (SELECT * FROM db2.'${table}' WHERE EQUALS(db1.'${table}'.geometry, db2.'${table}'.geometry));"
+	spatialite $dummydb "$attachdbs INSERT INTO db3.'${table}_added'   SELECT * from db2.'${table}' WHERE NOT EXISTS (SELECT * FROM db1.'${table}' WHERE EQUALS(db1.'${table}'.geometry, db2.'${table}'.geometry));
 	
 done <<< "$tables1"
 
