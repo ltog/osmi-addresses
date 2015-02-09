c67cf455c56eded665bd6109958d037887990f14
Lukas Toggenburger <lukas.toggenburger@htwchur.ch>
2015-02-09 23:20:57 +0100

    Copy databases and delete rows with identical ogc_fid and geometry

--- test/create_sqlite_diff.sh
+++ test/create_sqlite_diff.sh
@@ -3,11 +3,17 @@
 # Author:  Lukas Toggenburger
 # Website: https://github.com/ltog/osmi-addresses
 
+# Note on the output of spatialite: spatialite tries to be smart and sniffs the kind of output destination. If the output destination is a terminal, it will output some verbose information each time you run it. This is the reason why most (probably all) calls to spatialite have a pipe after them.
+
 description="This tool reads two spatialite files and creates a third containing the difference (deleted and newly added rows) of the tables of those two files."
 
+geometry_column='geometry' # name of the column where geometries are stored
 dummydb='dummydbdeleteme'
 old_suffix='_deleted'
 new_suffix='_added'
+tmpdir='/tmp/'
+p1=${tmpdir}db1 # path for temporary file for db1
+p2=${tmpdir}db2 # path for temporary file for db2
 
 # make sure exactly 3 arguments are given
 if [ $# -ne 3 ]; then
@@ -45,18 +51,40 @@ then
 	exit 4
 fi
 
-attachdbs="ATTACH DATABASE '$1' AS db1; ATTACH DATABASE '$2' AS db2; ATTACH DATABASE '$3' AS db3;"
+# create copies of the database files
+cp -f $1 $p1
+cp -f $2 $p2
+
+attach2dbs="ATTACH DATABASE '$p1' AS db1; ATTACH DATABASE '$p2' AS db2;"
+attach3dbs="ATTACH DATABASE '$p1' AS db1; ATTACH DATABASE '$p2' AS db2; ATTACH DATABASE '$3' AS db3;"
 
 # for each table write the differences to a new database
 while read -r table; do
 	echo "Processing table '$table' ..."
 
-	# create new geometry tables in db3
-	# variant 1: manually call AddGeometryColumn: http://stackoverflow.com/a/23366622 https://www.gaia-gis.it/spatialite-2.3.0/spatialite-sql-2.3.0.html#p16
-	# variant 2: clone table (use RecoverGeometryColumn (?))
+	# PHASE 1: erase identical rows of copied databases
+	echo "  removing identical rows in copies of databases..."
+
+	# create temporary tables for ids to be deleted
+	spatialite $dummydb "$attach2dbs DROP TABLE IF EXISTS db1.doomed;" | grep -ve '^$'
+	spatialite $dummydb "$attach2dbs DROP TABLE IF EXISTS db2.doomed;" | grep -ve '^$'
+	spatialite $dummydb "$attach2dbs CREATE TABLE db1.doomed (id INTEGER);" | grep -ve '^$'
+	spatialite $dummydb "$attach2dbs CREATE TABLE db2.doomed (id INTEGER);" | grep -ve '^$'
+
+	# load ids of rows to be deleted
+	spatialite $dummydb "$attach2dbs INSERT INTO db1.'doomed' (id) SELECT db1.${table}.ogc_fid FROM db1.$table INNER JOIN db2.$table ON db1.${table}.ogc_fid=db2.${table}.ogc_fid WHERE Equals(db1.${table}.${geometry_column},db2.${table}.${geometry_column})" | grep -ve '^$'
+	spatialite $dummydb "$attach2dbs INSERT INTO db2.'doomed' (id) SELECT db2.${table}.ogc_fid FROM db2.$table INNER JOIN db1.$table ON db1.${table}.ogc_fid=db2.${table}.ogc_fid WHERE Equals(db1.${table}.${geometry_column},db2.${table}.${geometry_column})" | grep -ve '^$'
+
+	# delete rows
+	spatialite $dummydb "$attach2dbs DELETE FROM db1.'$table' WHERE ogc_fid IN (SELECT id FROM db1.doomed)" | grep -ve '^$'
+	spatialite $dummydb "$attach2dbs DELETE FROM db2.'$table' WHERE ogc_fid IN (SELECT id FROM db2.doomed)" | grep -ve '^$'
+
+	# delete temporary tables
+	spatialite $dummydb "$attach2dbs DROP TABLE db1.'doomed'" | grep -ve '^$'
+	# PHASE 2
 
 	# get schema
-	schema=$(spatialite $1 ".schema $table" | grep 'CREATE TABLE' | grep "$table")
+	schema=$(spatialite $p1 ".schema $table" | grep 'CREATE TABLE' | grep "$table")
 
 	# get geometry type (extract whats written after "GEOMETRY"
 	geometry_type=$(echo $schema | sed -e 's/.*\"GEOMETRY\" \([A-Za-z]*\)[,)].*/\1/')
@@ -69,25 +97,26 @@ while read -r table; do
 	schema_new=$(echo $schema_without_geometry | sed -e "s/\(${table}\)/\1${new_suffix}/")
 
 	# get srid
-	srid=$(spatialite $1 "SELECT srid FROM geometry_columns WHERE f_table_name='$table';" | tail)
-
-	echo DEBUG: $schema_old
-	echo DEBUG: $schema_new
-	echo DEBUG: $geometry_type,
+	srid=$(spatialite $p1 "SELECT srid FROM geometry_columns WHERE f_table_name='$table';" | tail)
 
 	# create new tables in new file
-	spatialite $3 "$schema_old"
-	spatialite $3 "$schema_new"
+	spatialite $3 "$schema_old" | grep -ve '^$'
+	spatialite $3 "$schema_new" | grep -ve '^$'
 
 	# add geometry column
-	spatialite $3 "SELECT AddGeometryColumn('${table}${old_suffix}', 'geometry', $srid, '$geometry_type');"
-	spatialite $3 "SELECT AddGeometryColumn('${table}${new_suffix}', 'geometry', $srid, '$geometry_type');"
+	spatialite $3 "SELECT AddGeometryColumn('${table}${old_suffix}', '$geometry_column', $srid, '$geometry_type');" | grep -ve '1'
+	spatialite $3 "SELECT AddGeometryColumn('${table}${new_suffix}', '$geometry_column', $srid, '$geometry_type');" | grep -ve '1'
 
 	# calculate differences and write them to db3
-	spatialite $dummydb "$attachdbs INSERT INTO db3.'${table}${old_suffix}' SELECT * from db1.'${table}' WHERE NOT EXISTS (SELECT * FROM db2.'${table}' WHERE EQUALS(db1.'${table}'.geometry, db2.'${table}'.geometry));"
-	spatialite $dummydb "$attachdbs INSERT INTO db3.'${table}${new_suffix}' SELECT * from db2.'${table}' WHERE NOT EXISTS (SELECT * FROM db1.'${table}' WHERE EQUALS(db1.'${table}'.geometry, db2.'${table}'.geometry));"
+	echo "  searching for deleted geometries..."
+	spatialite $dummydb "$attach3dbs INSERT INTO db3.'${table}${old_suffix}' SELECT * from db1.'${table}' WHERE NOT EXISTS (SELECT * FROM db2.'${table}' WHERE EQUALS(db1.'${table}'.$geometry_column, db2.'${table}'.$geometry_column));" | grep -ve '^$'
+
+	echo "  searching for newly added geometries..."
+	spatialite $dummydb "$attach3dbs INSERT INTO db3.'${table}${new_suffix}' SELECT * from db2.'${table}' WHERE NOT EXISTS (SELECT * FROM db1.'${table}' WHERE EQUALS(db1.'${table}'.$geometry_column, db2.'${table}'.$geometry_column));" | grep -ve '^$'
 
 done <<< "$tables1"
 
 # delete dummy db
 rm $dummydb
+rm $p1
+rm $p2
