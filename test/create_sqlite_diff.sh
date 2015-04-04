63fd6bb9eb5d1faeaf7e74ef873a1ceb7e846fa4
Lukas Toggenburger <lukas.toggenburger@htwchur.ch>
2015-04-04 17:55:24 +0200

    [cosmetic] Fix indention; Remove old comment

--- test/create_sqlite_diff.sh
+++ test/create_sqlite_diff.sh
@@ -76,28 +76,28 @@ ensure_schemas_identical() {
 
 # clone the schema of a geometry table
 clone_geometry_table_schema() {
-		# assign variable names
-		local srctable=$1
-		local dsttable=$2
-		local srcdb=$3
-		local dstdb=$4
+	# assign variable names
+	local srctable=$1
+	local dsttable=$2
+	local srcdb=$3
+	local dstdb=$4
 
-		# get schema
-		schema=$(spatialite $srcdb ".schema $srctable" | grep 'CREATE TABLE' | grep "$srctable")
+	# get schema
+	schema=$(spatialite $srcdb ".schema $srctable" | grep 'CREATE TABLE' | grep "$srctable")
 
-		# get geometry type (extract what is written after "GEOMETRY"
-		geometry_type=$(echo "$schema" | sed -e 's/.*\"GEOMETRY\" \([A-Za-z]*\)[,)].*/\1/i')
+	# get geometry type (extract what is written after "GEOMETRY"
+	geometry_type=$(echo "$schema" | sed -e 's/.*\"GEOMETRY\" \([A-Za-z]*\)[,)].*/\1/i')
 
-		# adjust table names in 'CREATE TABLE ...' expressions by appending suffixes
-		schema_adjusted=$(echo "$schema" | sed -e "s/${srctable}/${dsttable}/g")
+	# adjust table names in 'CREATE TABLE ...' expressions by appending suffixes
+	schema_adjusted=$(echo "$schema" | sed -e "s/${srctable}/${dsttable}/g")
 
-		# create new tables in new file from adjusted schema
-		spatialite $dstdb "$schema_adjusted" 2>&1 | grep -ve '^$' | fgrep -v -e 'the SPATIAL_REF_SYS table already contains some row(s)'
+	# create new tables in new file from adjusted schema
+	spatialite $dstdb "$schema_adjusted" 2>&1 | grep -ve '^$' | fgrep -v -e 'the SPATIAL_REF_SYS table already contains some row(s)'
 
-		# read srid from the input file
-		srid=$(spatialite $srcdb "SELECT srid FROM geometry_columns WHERE f_table_name='$srctable';" | grep -ve '^$')
+	# read srid from the input file
+	srid=$(spatialite $srcdb "SELECT srid FROM geometry_columns WHERE f_table_name='$srctable';" | grep -ve '^$')
 
-		spatialite $dstdb "SELECT RecoverGeometryColumn('$dsttable', '$geometry_column', $srid, '$geometry_type');" | grep -ve '^$' | grep -v '^1$'
+	spatialite $dstdb "SELECT RecoverGeometryColumn('$dsttable', '$geometry_column', $srid, '$geometry_type');" | grep -ve '^$' | grep -v '^1$'
 }
 
 create_tmp_dbs_schemas() {
@@ -242,7 +242,7 @@ ensure_table_names_identical
 echo "Creating temporary database schemas..."
 create_tmp_dbs_schemas
 
-echo "Copying content into temporary databases..." # TODO: parallelisieren
+echo "Copying content into temporary databases..."
 fill_tmp_dbs
 
 echo "Removing duplicate geometries..."
