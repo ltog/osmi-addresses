4f4a38071d67341f4832bbb9acf07687cfbfbc30
Lukas Toggenburger <lukas.toggenburger@htwchur.ch>
2015-04-23 19:28:37 +0200

    Run on multiple .sqlite files in parallel (#55)

--- create_spatial_indices.sh
+++ create_spatial_indices.sh
@@ -1,22 +1,30 @@
 #!/bin/bash
 
-if [ $# -ne 1 ]; then
-	echo "Usage: $0 sqlitefile"
-	exit
-fi
-
 # http://www.sqlite.org/pragma.html#pragma_cache_size
 # http://www.sqlite.org/pragma.html#pragma_journal_mode journal_mode=MEMORY is maybe a small bit faster than =OFF
-prefix="PRAGMA synchronous=OFF; PRAGMA cache_size=-4000000; PRAGMA journal_mode=OFF; PRAGMA read_uncommitted=1;"
+spatialite_pragmas="PRAGMA synchronous=OFF; PRAGMA cache_size=-4000000; PRAGMA journal_mode=OFF; PRAGMA read_uncommitted=1;"
 
+parallel_options="--noswap"
 
-tables=$(spatialite $1 ".schema" | grep "CREATE TABLE '" | sed -e "s/^CREATE TABLE '\([^']*\)'.*$/\1/")
+if [ $# -ne 1 ]; then
+	echo "Usage: $0 DIR_WITH_SQLITE_FILES"
+	exit 1
+fi
 
-# can't call .schema after usage of $prefix for some unknown reason...
-# tables=$(spatialite $1 "$prefix .schema" | grep "CREATE TABLE '" | sed -e "s/^CREATE TABLE '\([^']*\)'.*$/\1/")
+#TODO: check if $1 is a directory
 
-while read -r table; do
-	# starting from version 4.2.0 we can call spatialite with the option '-silent' which reduces output
-	# see e.g. https://stackoverflow.com/questions/23579001/how-do-i-make-the-spatialite-banner-go-away-under-django-manage-py
-	spatialite $1 "$prefix BEGIN; SELECT CreateSpatialIndex('$table', 'GEOMETRY'); COMMIT;" > /dev/null
-done <<< "$tables"
+create_spatial_indices() {
+	local tables=$(spatialite $1 ".schema" | grep "CREATE TABLE '" | sed -e "s/^CREATE TABLE '\([^']*\)'.*$/\1/")
+	
+	# can't call .schema after usage of $spatialite_pragmas for some unknown reason...
+	# tables=$(spatialite $1 "$spatialite_pragmas .schema" | grep "CREATE TABLE '" | sed -e "s/^CREATE TABLE '\([^']*\)'.*$/\1/")
+	
+	while read -r table; do
+		# starting from version 4.2.0 we can call spatialite with the option '-silent' which reduces output
+		# see e.g. https://stackoverflow.com/questions/23579001/how-do-i-make-the-spatialite-banner-go-away-under-django-manage-py
+		spatialite $1 "$spatialite_pragmas BEGIN; SELECT CreateSpatialIndex('$table', 'GEOMETRY'); COMMIT;" > /dev/null
+	done <<< "$tables"
+}
+export -f create_spatial_indices
+
+parallel $parallel_options create_spatial_indices ::: $(realpath $1/*.sqlite)
