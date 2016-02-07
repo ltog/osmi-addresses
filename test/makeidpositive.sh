eef704407a0994e5cf2e90d2268b2eb25fecba3f
Lukas Toggenburger <lukas.toggenburger@htwchur.ch>
2016-02-07 10:08:09 +0100

    Broaden condition when usage instructions are printed

--- test/makeidpositive.sh
+++ test/makeidpositive.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ $# -eq 0  ]; then
+if [ $# -ne 1  ]; then
 	echo "Usage: $0 file.xml"
 	echo "$0 creates a new file pos-file.xml with all negative attributes id= and ref= set to their absolute value."
 	echo "$0 relies on 'sed' instead of a proper XML parser ('xmlstarlet' would probably be a fine choice) but should work fine as long the id= and ref= attributes have no linebreaks in them."
